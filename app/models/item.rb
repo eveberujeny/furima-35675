830f065bf6c1b40a57d15efdac2c7e58f8584e42
eveberujeny <83558525+eveberujeny@users.noreply.github.com>
2021-05-30 15:07:06 +0900

    ルボコップ実施

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,20 +3,14 @@ class Item < ApplicationRecord
     validates :name
     validates :description
     validates :price
-    validates :category_id
-    validates :status_id
-    validates :burden_id
-    validates :source_id
-    validates :shipping_day_id
     validates :image
-  end
-
-  with_options numericality: { other_than: 1 } do
-    validates :category_id
-    validates :status_id
-    validates :burden_id
-    validates :source_id
-    validates :shipping_day_id
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :burden_id
+      validates :source_id
+      validates :shipping_day_id  
+    end  
   end 
 
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
